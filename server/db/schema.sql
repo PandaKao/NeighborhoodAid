189a3c399f973419d80944cbfb54bf44b605674f
Justin Kao <justinkao010@gmail.com>
2024-10-08 21:16:16 -0700

    fixed authService still need to add .env stuff

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS change_this_db;
-CREATE DATABASE change_this_db;
+DROP DATABASE IF EXISTS neighborhoodaid_db;
+CREATE DATABASE neighborhoodaid_db;
 
 \c change_this_db;
 -- need this extension for geometry for longitude/latitude
