2d59727d22c0c2cde824d49c2ff482ce8209bf6a
Justin Kao <justinkao010@gmail.com>
2024-10-09 10:24:42 -0700

    added header to nominatim axios request

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS neighborhoodaid_db;
 CREATE DATABASE neighborhoodaid_db;
 
-\c change_this_db;
+\c neighborhoodaid_db;
 -- need this extension for geometry for longitude/latitude
 CREATE EXTENSION IF NOT EXISTS postgis;
