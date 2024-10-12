0a9abdfb7cf579100e4dd799a012ac07cd128269
Justin Kao <justinkao010@gmail.com>
2024-10-12 15:20:30 -0700

    changed dialect to postgres

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,13 +3,13 @@ import "dotenv/config";
 
 const sequelize =
   process.env.NODE_ENV === "production"
-    ? new Sequelize(process.env.DATABASE_URL)
+    ? new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'})
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASS,
         {
-          host: process.env.HOST,
+          host: "localhost",
           dialect: "postgres",
         },
       );
